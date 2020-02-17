f70043a9fa47dc99190bde8b6965a00f549bb779
padmasumesh <padmasumesh828@gmail.com>
2020-02-17 21:01:58 +0530

    third commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num[50],s,n,avg;
+cout<<"\ Enter the array limit";
+cin>>n;
+for(i=0;i<n;i++)
+{
+cin>>num[i];
+sum=sum+a[i];
+}
+avg=s/n;
+cout<<s<<"\n"<<avg<<"\n";
+return 0;
+}
